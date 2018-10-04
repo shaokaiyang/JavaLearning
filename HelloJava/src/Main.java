a2ad9cd03fca78ac0a576e4ac1775b0227bb51b7
shaokaiyang <kaiyang2013@outlook.com>
2018-10-05 01:33:41 +0800

    Java

--- HelloJava/src/Main.java
+++ HelloJava/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
         System.out.println("Let's starting !");
+        System.out.println("Though Java is complex, I like it .");
     }
 }
